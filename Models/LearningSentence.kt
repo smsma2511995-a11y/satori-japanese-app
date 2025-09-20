f459ebd98e33be833cee630f30b7086258c42b86
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-20 17:50:38 +0300

    Create LearningSentence.kt

---
+++ Models/LearningSentence.kt
@@ -0,0 +1,9 @@
+package com.satori.japanese.learning.models
+
+data class LearningSentence(
+    val japanese: String,
+    val translation: String,
+    val breakdown: Map<String, String>,
+    val category: String,
+    val level: String
+)
